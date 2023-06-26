c1aae16c70b72c127fd069e370bbd4c88635f5c5
高慧杰 <9431681+gao-huijie@user.noreply.gitee.com>
2023-06-26 15:09:13 +0800

    更改

--- zbcirlce-third-service/src/main/java/com/zb/third/controller/OssController.java
+++ zbcirlce-third-service/src/main/java/com/zb/third/controller/OssController.java
@@ -43,12 +43,9 @@ public class OssController {
     private String accessId;
     @RequestMapping("/oss/policy")
     public RestResponse policy() {
-        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
-        // 填写Bucket名称，例如examplebucket。
-        // 填写Host地址，格式为https://bucketname.endpoint。
+
         String host = "https://" + bucket + "." + endPoint;
-        // 设置上传回调URL，即回调服务器地址，用于处理应用服务器与OSS之间的通信。OSS会在文件上传完成后，把文件上传信息通过此回调URL发送给应用服务器。
-//        String callbackUrl = "https://192.168.0.0:8888";
+
         String format = new SimpleDateFormat("yyyy-MM-dd").format(new Date());
 
         // 设置上传到OSS文件的前缀，可置空此项。置空后，文件将上传至Bucket的根目录下。
